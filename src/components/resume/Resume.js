286675d7ba2f6ee19b9662a26cfbc5a6ec58afca
Nafaska <viktoria.kovalenko7@gmail.com>
2021-03-09 16:15:47 +0100

    added CV info

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Resume.css";
-import CV_Viktoriia_Kovalenko from "../../documents/CV_Viktoriia_Kovalenko.pdf";
 import { ReactComponent as DownloadPDFIcon } from "../../assets/downloadPdf.svg";
 
 const Resume = React.forwardRef((props, ref) => {
   return (
     <div className="resume" ref={ref}>
       <div>Download my CV</div>
-      <a href={CV_Viktoriia_Kovalenko} target="_blank" rel="noreferrer">
+      <a
+        href="/documents/CV_Viktoriia_Kovalenko.pdf"
+        target="_blank"
+        rel="noreferrer"
+      >
         <DownloadPDFIcon />
       </a>
     </div>
