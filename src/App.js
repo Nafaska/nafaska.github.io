88d45425569401c9673f94ccf9850191428f7aef
Nafaska <viktoria.kovalenko7@gmail.com>
2021-03-14 19:00:22 +0100

    add google analytics

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const contactsSectionRef = useRef(null);
 
   console.log(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+  console.log(process.env.REACT_APP_TEMPLATE_ID);
 
   useEffect(() => {
     console.log(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
